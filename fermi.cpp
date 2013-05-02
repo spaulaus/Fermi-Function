ef734c4ccd6db5c55ca5523e1495c488f6da894f
S.V. Paulauskas <stanpaulauskas@gmail.com>
2013-05-02 11:54:26 -0400

    Now actually calculating the fermi integral

--- fermi.cpp
+++ fermi.cpp
@@ -13,8 +13,13 @@ using namespace std;
 
 PhysConstants consts;
 
+//Some of the useful constants to be used
+double c = consts.GetConstant("c").GetValue();
+double hbar = consts.GetConstant("hbar").GetValue();
+double eMass = consts.GetConstant("electronMass").GetValue() / (c*c);
+
 //Gamma function for complex argument G(a+bi) returns |G|**2, a la Lanczos!
-complex<double> Gamma(double a, double b) {
+complex<double> Gamma(const double &a, const double &b) {
     double gg = 7.0;
     complex<double> z(a,b), cG, x, t;
     double c[9] = {0.99999999999980993, 676.5203681218851, -1259.1392167224028,
@@ -33,11 +38,11 @@ complex<double> Gamma(double a, double b) {
     return(cG);
 }
 
-//Courtesy of M. G. Bertolli - that fucking dick.
+//Courtesy of M. G. Bertolli - that fucking dick. 
 //units on w = MeV and Fermi = unitless
-//Beta decay goes from Q to ~0
-double Fermi(int z, int a, double w) {
-    w /= consts.GetConstant("c").GetValue()*consts.GetConstant("hbar").GetValue();
+double CalcFermiFunction(const int &z, const int &a, const double &w) {
+    double energy = w;
+    energy /= c*hbar;
     double r0 = 1.2e-15; // m
     double R = r0*pow(a, 1./3.);
     double p = sqrt(w*w-1.);
@@ -49,14 +54,33 @@ double Fermi(int z, int a, double w) {
 	     exp(M_PI*y)*norm(Gamma(gamma,y))/norm(Gamma(2*gamma+1.0,0)) ) ;
 }
 
-int main() {
-    double a = 77;
-    double z = 29;
-    double qbeta = -10.150;
-    double en = 0.269929;
+double CalcFermiIntegral(void) {
+    //Some of the initial parameters
+    double a = 76;
+    double z = 30;
+    double qbeta = 10.150;
     double step = 0.01;
+    
+    double integral = 0; 
+    for(double i = 1+step; i < qbeta; i+=step) {
+        double fermiFunc = CalcFermiFunction(z,a,-qbeta+i);
+        double pe = sqrt(2*eMass*i);
+        double diff = pow(qbeta-i,2.0);
+
+        cout << i << " " << fermiFunc << " " << " " << pe << " " 
+             << diff << " " << endl;
+        integral += fermiFunc*pe*i*pow(qbeta-i, 2.0); 
+    }
+
+    cout << "Ingegral = " << integral << "  1/emass**5 = " << pow(eMass,5.) 
+         << endl;
+    return(integral/pow(eMass,5.));
+}
+
+int main() {
+    double en = 0.269929;
 
     //for(double i = qbeta+step; i < 0; i += step)
     //    cout << i << " " << Fermi(z,a,qbeta-i) << endl;
-    cout << en << " " << log10(Fermi(z,a,qbeta+en)) << endl;
+    cout << en << " " << log10(CalcFermiIntegral()) << endl;
 }

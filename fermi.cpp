8f186e670d412e65c2ec26aa55d10cebdadc27a9
S.V. Paulauskas <stanpaulauskas@gmail.com>
2013-05-03 16:51:54 -0400

    Restarting with the units, just have the raw varaibles/funcs in there

--- fermi.cpp
+++ fermi.cpp
@@ -15,7 +15,7 @@ PhysConstants consts;
 
 //Some of the useful constants to be used
 double c = consts.GetConstant("c").GetValue();
-double hbar = consts.GetConstant("hbar").GetValue();
+double hbar = consts.GetConstant("hbar").GetValue()/1e6;
 double eMass = consts.GetConstant("electronMass").GetValue() / (c*c);
 
 //Gamma function for complex argument G(a+bi) returns |G|**2, a la Lanczos!
@@ -38,42 +38,44 @@ complex<double> Gamma(const double &a, const double &b) {
     return(cG);
 }
 
-//Courtesy of M. G. Bertolli - that fucking dick. 
+//Courtesy of M. G. Bertolli
 //units on w = MeV and Fermi = unitless
 double CalcFermiFunction(const int &z, const int &a, const double &w) {
-    double energy = w;
+    double energy = w; // MeV
     double r0 = 1.2e-15; // m
-    double R = r0*pow(a, 1./3.);
-    double p = sqrt(w*w-1.);
+    double R = (r0*pow(a, 1./3.)); // 1/MeV
+    double p = sqrt(energy*energy-1.); // MeV
     double fineStructure = consts.GetConstant("fineStructure").GetValue();
     double gamma = sqrt(1.-pow(fineStructure*z,2));
-    double y = (fineStructure*z*w)/p;
+    double y = (fineStructure*z*energy)/p;
 
     return ( 2*(1.+gamma)*pow(2*p*R, -2*(1.-gamma))*
 	     exp(M_PI*y)*norm(Gamma(gamma,y))/norm(Gamma(2*gamma+1.0,0)) ) ;
 }
 
+//comes from Langanke
 double CalcFermiIntegral(void) {
     //Some of the initial parameters
-    double a = 76;
+    double a = 77;
     double z = 29;
-    double qbeta = 10.150/(eMass*pow(c,2)); //Starts in MeV to convert
+    double qbeta = 10.28; // units me*c*c 
     double step = 0.01;
     
     double integral = 0; 
     for(double w = 1+step; w < qbeta; w+=step) {
         double fermiFunc = CalcFermiFunction(z+1,a,w);
-        double pe = sqrt(pow(w,2.0)-1);
+        double pe = sqrt(w*w-1);  // units me*c
         double diff = pow(qbeta-w,2.0);
 
-        // cout << w << " " << fermiFunc << " " << " " << pe << " " 
-        //      << diff << " " << endl;
-        integral += fermiFunc*pe*w*diff;
+        cout << w << " " << fermiFunc << " " << " " << pe << " " 
+             << diff << " " << endl;
+        integral += w*pe*diff*fermiFunc;
     }
     return(integral);
 }
 
 int main() {
-    cout << "f = " << CalcFermiIntegral() << endl 
-         << "log(f) = " << log10(CalcFermiIntegral()) << endl;;
+    double f = CalcFermiIntegral();
+    cout << "f = " << f << endl 
+         << "log(f) = " << log10(f) << endl;;
 }
